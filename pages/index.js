e85ae3f9ea42c27dd4ef032114f85f96589420d7
Csokán Pál András <csokan.andras87@protonmail.ch>
2023-02-26 10:15:14 +0100

    Register module under Image logo

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,9 @@ export default function App() {
                 height={189}
                 priority
               />
+              <Register />
           </div>
-          <Register />
+          
         </main>
         
       </>
