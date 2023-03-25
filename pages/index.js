5052730333f8d21d92f7d9595d9848330b073e7b
Ciocan Pál András <csokan.andras87@protonmail.ch>
2023-03-25 12:33:06 +0100

    Codemod upgrade next-legacy-image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Image from "next/legacy/image"
 import styles from '../styles/Home.module.scss'
 import Register from './register'
 import { ApolloProvider } from '@apollo/client';
