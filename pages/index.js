9e9e947b76aeeebf173f3b066bc167c042e14bc8
Csokán Pál András <csokan.andras87@protonmail.ch>
2023-03-28 10:46:21 +0200

    Fejlec + logo beállítás

--- pages/index.js
+++ pages/index.js
@@ -16,21 +16,25 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <main className={styles.main}>
-        <div className={styles.logo}>
-            <Image
-                src='/logo-hu.png'
-                alt="Logo"
-                width={415}
-                height={189}
-                priority
-            />
+        <div className={styles.fejlec}>
+          <div className={styles.logo}>
+              <Image
+                  src='/logo-hu.png'
+                  alt="Logo"
+                  width={415}
+                  height={189}
+                  priority
+              />
+          </div>
         </div>
       </main>
       <ApolloProvider client={client}>
         {/* <Query /> */}
         <Register />
       </ApolloProvider>
+      
       </>
   )
 }
