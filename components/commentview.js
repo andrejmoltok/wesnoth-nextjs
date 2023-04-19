b4374b2ff33bc2b743ffa22a38ddbfd60494bc2a
Csokán Pál András <csokan.andras87@protonmail.ch>
2023-04-19 20:41:23 +0200

    Commenting out unneeded functions temporarily

--- components/commentview.js
+++ components/commentview.js
@@ -1,14 +1,13 @@
 import styler from '../styles/CommentView.module.css';
 import Image from 'next/image';
 import Cookies from 'universal-cookie';
-import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFeather, faCalendarDays, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { useQuery, useMutation } from '@apollo/client';
 import { DocumentRenderer } from '@keystone-6/document-renderer';
 import { QUERY_POST_COMMENTS } from '../gql/CommentView/QUERY_POST_COMMENTS';
-import { DELETE_COMMENT } from '../gql/CommentView/COMMENT_DELETE';
-import { COMMENT_DISCONNECT } from '../gql/CommentView/COMMENT_DISCONNECT';
+// import { DELETE_COMMENT } from '../gql/CommentView/COMMENT_DELETE';
+// import { COMMENT_DISCONNECT } from '../gql/CommentView/COMMENT_DISCONNECT';
 import { COMMENT_REPLACE } from '../gql/CommentView/COMMENT_REPLACE';
 
 export default function CommentView({postID}) {
@@ -25,24 +24,15 @@ export default function CommentView({postID}) {
 
     const {author, name, race, races, image, url, createdAt, content, document, id: commentID, id: userID} = data?.post?.comments || {};
 
-    // check name for comment associating
-    const [getID, setGetID] = useState("");
-
     // replace comment mutation
     const [replace, { loading: replaceLoading, error: replaceError, data: replaceData }] = useMutation(COMMENT_REPLACE);
-    // delete Comment mutation
-    const [deleteComment, { loading: deleteLoading, error: deleteError, data: deleteData }] = useMutation(DELETE_COMMENT);
-    // disconnect Comment mutation
-    const [disconComment, { loading: disconLoading, error: disconError, data: discondata }] = useMutation(COMMENT_DISCONNECT);
+    // // delete Comment mutation
+    // const [deleteComment, { loading: deleteLoading, error: deleteError, data: deleteData }] = useMutation(DELETE_COMMENT);
+    // // disconnect Comment mutation
+    // const [disconComment, { loading: disconLoading, error: disconError, data: discondata }] = useMutation(COMMENT_DISCONNECT);
 
     const cookies = new Cookies();
-  
-    useEffect(() => {
-      data?.post?.comments.map((v,i) => {
-        if (cookies.get('id') === v?.author?.id) {
-        setGetID(v?.author?.id)}})
-    } ,[data]);
-    
+      
     return (
         <>
         <div className={styler.commentView}>
@@ -62,7 +52,7 @@ export default function CommentView({postID}) {
                     <FontAwesomeIcon icon={faCalendarDays} size="sm" /> Dátum: {v?.createdAt?.slice(0,10)} {v?.createdAt.slice(11,19)}
                   </div>
                 </div>
-                {(getID === v?.author?.id) && <div className={styler.deleteIcon}>
+                {(v?.author?.id === cookies.get('id')) && <><div className={styler.deleteIcon}>
                   <div onClick={() => {replace({variables: {
                     "where": {"id": v?.id}, 
                     "data": {
@@ -73,10 +63,7 @@ export default function CommentView({postID}) {
                         }]
                       }]
                     }
-                  }})}}>
-                          <FontAwesomeIcon icon={faTrashCan} /></div></div>}
-                {/*disconComment({variables: {"where": {"id": postID}, "data": {"comments":{"disconnect":[{"id": v?.id}]}}}}),
-                        deleteComment({variables: {"where": {"id": v?.id}}}) */}
+                  }})}}> <FontAwesomeIcon icon={faTrashCan} /> </div></div></>}
             </div>
             <div className={styler.document}>
               <DocumentRenderer document={v?.content?.document}/>
@@ -88,3 +75,6 @@ export default function CommentView({postID}) {
         </>
     )
 }
+
+{/*disconComment({variables: {"where": {"id": postID}, "data": {"comments":{"disconnect":[{"id": v?.id}]}}}}),
+  deleteComment({variables: {"where": {"id": v?.id}}}) */}
