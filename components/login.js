c9eea34669afbf0e96f3223d22b48ea977ebcd8e
Csokán Pál András <csokan.andras87@protonmail.ch>
2023-05-18 21:11:21 +0200

    Tempered with Login mutaiton and componenet

--- components/login.js
+++ components/login.js
@@ -1,7 +1,7 @@
 import styles from '../styles/Register.module.css';
-import client from '../apollo-client';
 import Cookies from 'universal-cookie';
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLazyQuery, useMutation } from '@apollo/client';
 import { EMAIL_CHECK } from '../gql/Login/EMAIL_CHECK';
 import { LOGIN } from '../gql/Login/LOGIN';
 import { LOGGER } from '../gql/Login/LOGGER';
@@ -14,88 +14,69 @@ export default function Login({handleShowRegister, handleShowLogin}) {
     const [password, setPassword] = useState("");
     // email validator
     const [errorEmailMsg, setErrorEmailMsg] = useState('');
-    // set emailData
-    const [emaildata, setEmaildata] = useState(null);
 
-    async function verify() {
+    // EMAIL_CHECK query
+    const [emailCheck, { data: emailData, loading: emailLoading, error: emailError }] = useLazyQuery(EMAIL_CHECK);
+
+    const { id, userRole, name } = emailData?.user || {};
+
+    // Authenticate user with password
+    const [login, { data: authData, loading: authLoading, error: authError }] = useMutation(LOGIN);
+     
+    const { sessionToken, message } = authData?.authenticateUserWithPassword || {};
+         
+    // Log the Login activity
+    const [logger, { data: loggerData, loading: loggerLoading, error: loggerError }] = useMutation(LOGGER);
+
+
+    useEffect(() => {
+      console.log("");
+    }, [email]);
+    // verify EMAIL from form
+    // then handleLogin
+    function verify(e) {
+      e.preventDefault();
       // Verify email
-      const { data: emailData } = await client.query({
-        query: EMAIL_CHECK,
-        variables: {
-          where: {
-            email: email,
-          },
-        },
-      });
+      emailCheck({variables: { where: { email: email }}});
       
-      // const { id, userRole, name } = emailData?.user || {};
-
-      if (!emailData) {
-
+      if (emailData?.user?.userRole === "Pending") {
+        setErrorEmailMsg('A fiók nincs aktiválva');
+        return;
       } else {
-        setEmaildata(emailData);
-        setErrorEmailMsg(emailData?.userRole === 'Pending' ? 'A fiók nincs aktiválva' : '');
+        setErrorEmailMsg('');
+        login({variables: { email: email, password: password }});
+        if (emailData) {
+          logger({variables: { data: { who: emailData?.user?.name, what: [{ type: 'paragraph', children: [{ text: 'Bejelentkezett' }]}]}}});
+        
+        // if (authData?.authenticateUserWithPassword.message) {
+        //   // Handle error here, such as displaying an error message
+        //   return `Error: ${message}`;
+        // };
+
+        const cookies = new Cookies();
+        cookies.set('id', emailData?.user?.id, {
+          path: '/',
+        });
+        cookies.set('keystonejs-session', sessionToken, {
+          path: '/',
+        });
+        }
+
+        setEmail('');
+        setPassword('');
       }
     };
 
-    const handleLogin = async (e) => {
-      e.preventDefault();
-
-    // Authenticate user with password
-    const { data: authData } = await client.mutate({
-      mutation: LOGIN,
-      variables: {
-        email: email,
-        password: password,
-      },
-    });
-
-    if (!authData?.authenticateUserWithPassword) {
-      // Handle error here, such as displaying an error message
-      return;
-    }
-
-    const { sessionToken } = authData?.authenticateUserWithPassword || {};
-    
-    // Log activity
-    const { data: loggerData } = await client.mutate({
-      mutation: LOGGER,
-      variables: {
-        data: {
-          who: emaildata.user.name,
-          what: [
-            {
-              type: 'paragraph',
-              children: [{ text: 'Bejelentkezett' }],
-            },
-          ],
-        },
-      },
-    });
-
-    // Set cookies instance
-    const cookies = new Cookies();
-    cookies.set('id', emaildata.user.id, {
-      path: '/',
-    });
-    cookies.set('keystonejs-session', sessionToken, {
-      path: '/',
-    });
-
-    client.clearStore();
-  }
-
     return (
         <>
-        
         <div className={styles.register}>
           <form className={styles.form}
-            onSubmit={handleLogin}>
+            onSubmit={verify}>
           <div><label htmlFor="email" className={styles.dist}>Email:</label></div>
-          <input type="email" name="email" onChange={(e) => {setEmail(e.target.value)}} onBlur={() => {verify()}} className={styles.email}/>
+          <input type="email" name="email" onChange={(e) => {setEmail(e.target.value)}} className={styles.email}/>
           {errorEmailMsg === '' ? null :
                 <span style={{
-                        fontWeight: 'thin',
+                        fontWeight: 'thin', 
                         color: 'red',
                 }}>{errorEmailMsg}</span>}
           <div><label htmlFor="password" className={styles.dist}>Jelszó:</label></div>
